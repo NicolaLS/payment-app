dc6046cff8854811a8e4417e4408070614c99d18
NicolaLS <sus.nym@proton.me>
2025-10-29 06:53:14 -0600

    fix: Start Koin on IOS

--- papp/composeApp/src/iosMain/kotlin/xyz/lilsus/papp/MainViewController.kt
+++ papp/composeApp/src/iosMain/kotlin/xyz/lilsus/papp/MainViewController.kt
@@ -1,5 +1,20 @@
 package xyz.lilsus.papp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatformTools
+import xyz.lilsus.papp.di.nwcModule
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    ensureKoin()
+    App()
+}
+
+private fun ensureKoin() {
+    val alreadyStarted = runCatching { KoinPlatformTools.defaultContext().get() }.isSuccess
+    if (!alreadyStarted) {
+        startKoin {
+            modules(nwcModule)
+        }
+    }
+}
