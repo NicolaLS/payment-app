ba936ca09b0c3badf70af11877b2b9f346183409
NicolaLS <sus.nym@proton.me>
2025-11-23 13:40:25 -0600

    ui: Add navigation bars padding

--- papp/composeApp/src/commonMain/kotlin/xyz/lilsus/papp/presentation/settings/PaymentsSettingsScreen.kt
+++ papp/composeApp/src/commonMain/kotlin/xyz/lilsus/papp/presentation/settings/PaymentsSettingsScreen.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -93,9 +95,11 @@ fun PaymentsSettingsScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(scrollState)
                 .padding(padding)
-                .padding(horizontal = 16.dp, vertical = 24.dp),
+                .consumeWindowInsets(padding)
+                .navigationBarsPadding()
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.Top
         ) {
             Surface(
