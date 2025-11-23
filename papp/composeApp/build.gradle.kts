bc19c336de96f04c96ae985fea44222fd4939eaf
NicolaLS <sus.nym@proton.me>
2025-11-23 15:08:37 -0600

    Bump versionCode for initial closed testing

--- papp/composeApp/build.gradle.kts
+++ papp/composeApp/build.gradle.kts
@@ -81,7 +81,7 @@ android {
         applicationId = "xyz.lilsus.papp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         ndk {
             //noinspection ChromeOsAbiSupport
