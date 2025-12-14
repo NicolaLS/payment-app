d61aa3bf80f5101ed1737459e56e7c324446c296
NicolaLS <sus.nym@proton.me>
2025-12-14 11:30:23 +0100

    chore/android: Bump versionCode for release

--- papp/composeApp/build.gradle.kts
+++ papp/composeApp/build.gradle.kts
@@ -87,7 +87,7 @@ android {
         applicationId = "xyz.lilsus.papp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
         ndk {
             //noinspection ChromeOsAbiSupport
