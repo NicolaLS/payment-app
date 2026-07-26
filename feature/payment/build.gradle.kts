ca315ac9f8b204827cdbb19a70efc58b1cfbe988
Nicola Susca <hello@nicolasusca.com>
2026-07-26 13:17:03 -0600

    refactor(payment): extract session transaction UI

--- feature/payment/build.gradle.kts
+++ feature/payment/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin {
             implementation(libs.bitcoin.kmp)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.foundation)
+            implementation(libs.compose.material.icons.extended)
             implementation(libs.compose.material3)
             implementation(libs.compose.runtime)
             implementation(libs.compose.ui)
