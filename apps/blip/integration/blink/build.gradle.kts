755250bc4de67b81ae0cfc7d2fc85fddf5b8254d
Nicola Susca <hello@nicolasusca.com>
2026-07-26 12:01:24 -0600

    refactor(blip): configure Blink Apollo service

--- apps/blip/integration/blink/build.gradle.kts
+++ apps/blip/integration/blink/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("xyz.lilsus.raylsuite.kmp.library")
+    alias(libs.plugins.apollo)
     alias(libs.plugins.kotlinSerialization)
 }
 
@@ -11,12 +12,42 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":core:payment"))
+            implementation(libs.apollo.runtime)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.multiplatform.settings)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
             implementation(libs.multiplatform.settings.test)
+        }
+    }
+}
+
+apollo {
+    service("blink") {
+        val blinkSchemaFile = file(
+            "src/commonMain/graphql/xyz/lilsus/blip/integration/blink/graphql/schema.graphqls"
+        )
+
+        packageName.set("xyz.lilsus.blip.integration.blink.graphql")
+        schemaFile.set(blinkSchemaFile)
+        generateAsInternal.set(true)
+        mapScalar("LnPaymentRequest", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar("WalletId", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar("PaymentHash", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar(
+            "LnPaymentPreImage",
+            "kotlin.String",
+            "com.apollographql.apollo.api.StringAdapter"
+        )
+        mapScalar("Memo", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar("ContactAlias", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar("ContactHandle", "kotlin.String", "com.apollographql.apollo.api.StringAdapter")
+        mapScalar("SatAmount", "kotlin.Long", "com.apollographql.apollo.api.LongAdapter")
+        mapScalar("SignedAmount", "kotlin.Long", "com.apollographql.apollo.api.LongAdapter")
+        introspection {
+            endpointUrl.set("https://api.blink.sv/graphql")
+            schemaFile.set(blinkSchemaFile)
         }
     }
 }
